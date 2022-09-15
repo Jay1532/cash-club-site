8fed691de4f5b4eae7a7f88e77e037e4c6a4f847
Jay Bhalala <bhalalajay722@gmail.com>
2022-09-15 00:30:03 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
   return (
     <div>
       <Home/>
-      <Clubs/>
+      {/* <Clubs/>
       <Donate/>
       <About/>
-      <Events/>
+      <Events/> */}
     </div>
   );
 }
